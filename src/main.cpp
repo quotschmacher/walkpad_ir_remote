c17561152c62463329e197ffc881f875678b47d2
quotschmacher <sven.kicherer@yahoo.com>
2024-11-07 14:03:30 +0100

    WifiManager und OTA hinzugefuegt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <WiFiManager.h>
+#include <ArduinoOTA.h>
+
+WiFiManager wifiManager;
 
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    wifiManager.setCountry("DE");
+    bool res = wifiManager.autoConnect("IrRemoteEsp");
+
+    ArduinoOTA.begin(WiFi.localIP(), "IrRemoteEsp", "password", InternalStorage);
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    ArduinoOTA.poll();
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+/*int myFunction(int x, int y) {
+    return x + y;
+}*/
